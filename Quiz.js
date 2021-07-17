dba498473ca2694ab8e1dabe8b1377e7f096c0c6
lalalalisa-k <programmerasari@gmail.com>
2021-07-17 17:59:24 +0900

    Add:カウント機能

--- Quiz.js
+++ Quiz.js
@@ -9,11 +9,6 @@ const quizOptionsContainer = document.getElementById('answers-container');
 const loader = document.getElementById('loader-message');
 const title = document.getElementById('title-message');
 const start = document.getElementById('start-button');
-
-const acceptingAnswers = true;
-let currentNum = 0;
-let isAnswered;
-let questionCounter = 0;
 let score = 0;
 
 class Quiz {
@@ -24,10 +19,6 @@ class Quiz {
   getQuestion(index) {
     return this.quizzes[index - 1].question;
   }
-/*
-  getNumOfQuiz(index) {
-    return this.quizzes.length[index - 1];
-  }*/
 
   getQuizCategory(index) {
     return this.quizzes[index - 1].category;
@@ -46,8 +37,7 @@ class Quiz {
   }
 }
 
-start.addEventListener('click', (index) => {
-  //upload();
+start.addEventListener('click', () => {
 
   displayLoading();
   start.style.display = "none";
@@ -57,10 +47,9 @@ start.addEventListener('click', (index) => {
       removeLoading();
       return response.json();
     })
-    .then((quizData, index) => {
+    .then((quizData) => {
       const quizInstance = new Quiz(quizData);
-      //getNewQuestion(quizInstance, index);
-      setQuiz(quizInstance, index);
+      setQuiz(quizInstance, 1);
     })
     .catch((e) => {
       console.log(e)
@@ -80,23 +69,19 @@ const removeLoading = () => {
   title.innerHTML = "";
 }
 
-const getNewQuestion = (quizInstance, index) => {
-//Object.keys(shuffledChoices).forEach(function (key) {
-  setQuiz(quizInstance, index);
-};
-/*
-const checkAnswer = (quizInstance,optionBtn) => {
-  if (optionBtn.innerHTML = quizInstance.getCorrectanswer(1)){
-    optionBtn.classList.add('correct');
-  } else{
-    optionBtn.classList.add('wrong');
-  }
+const doReload = () => {
+  window.location.reload();
+}
 
-//  if (isAnswered) {
-//    return;
-//  }
-//  isAnswered = true;
-} */
+const getNewQuestion = (quizInstance, index) => {
+  if (index - 1 === quizInstance.quizzes.length)
+  {
+    finishQuiz();
+  } else
+  {
+    setQuiz(quizInstance, index);
+  }
+};
 
 const shuffle = (arr) => {
   for (let i = arr.length - 1; i > 0; i--)
@@ -110,16 +95,15 @@ const shuffle = (arr) => {
 const setQuiz = (quizInstance, index) => {
   const answerLabel = document.getElementById('answers-container');
   quizNumber.innerHTML = `問題${index}`;
-  quizGenre.innerHTML = `[ジャンル]${quizInstance.getQuizCategory(1)}`;
-  quizDifficulty.innerHTML = `[難易度]${quizInstance.getQuizDifficulty(1)}`;
-  quizQuestion.innerHTML = `【クイズ】${quizInstance.getQuestion(1)}`;
+  quizGenre.innerHTML = `[ジャンル]${quizInstance.getQuizCategory(index)}`;
+  quizDifficulty.innerHTML = `[難易度]${quizInstance.getQuizDifficulty(index)}`;
+  quizQuestion.innerHTML = `【クイズ】${quizInstance.getQuestion(index)}`;
   quizOptionsContainer.innerHTML = '';
 
   const quizOptions = [];
-  quizOptions.push(quizInstance.getCorrectanswer(1));
-  quizOptions.push(...quizInstance.getIncorrectanswers(1));
+  quizOptions.push(quizInstance.getCorrectanswer(index));
+  quizOptions.push(...quizInstance.getIncorrectanswers(index));
 
-  isAnswered = false;
   const shuffledChoices = shuffle(quizOptions);
 
   Object.keys(shuffledChoices).forEach(function (key) {
@@ -128,54 +112,36 @@ const setQuiz = (quizInstance, index) => {
     const optionBtn = document.createElement("button");
     btnContainer.appendChild(optionBtn);
     optionBtn.innerHTML = quizOptions[key];
-    optionBtn.addEventListener('click', (quizInstance,index) => {
-      //answerLabel.removeChild(btnContainer);
-      //btnContainer.removeChild(optionBtn);
-      //quizOptionsContainer.innerHTML = ''; 
- 
+    optionBtn.addEventListener('click', () => {
+      answerLabel.removeChild(btnContainer);
+      btnContainer.removeChild(optionBtn);
+      quizOptionsContainer.innerHTML = '';
+
+      if (optionBtn.innerHTML === quizInstance.getCorrectanswer(index))
+      {
+        score = score + 1;
+      }
+      index = index + 1;
       getNewQuestion(quizInstance,index);
-      //checkAnswer(optionBtn);
-      /*
-      if (optionBtn.innerHTML = quizInstance.getCorrectanswer(i))
-      {
-        optionBtn.classList.add('correct');
-      } else
-      {
-        optionBtn.classList.add('wrong');
-      }*/
-      index++;
+
     });
     btnContainer.appendChild(optionBtn);
     answerLabel.appendChild(btnContainer);
   })
-
-  console.log(quizInstance);
-  console.log(quizInstance.quizzes);
-  console.log("[問題]" + index);
-
-  console.log("正解"+quizInstance.getCorrectanswer(1));
-  console.log(quizInstance.getIncorrectanswers(1));
-  console.log(quizzess.getNumOfQuiz(1));
-
-}
-
-const upload = () => {
-  //displayLoading();
-  loader.innerHTML = "少々お待ちください";
-  title.innerHTML = "取得中";
-
-  removeLoading();
 }
 
 const finishQuiz = () => {
-  title.innerText = 'あなたの正解数は${}です！！';
-  loader.innerText = '再度チャレンジしたい場合は以下をクリック！';
+  quizNumber.innerHTML = '';
+  quizGenre.innerHTML = '';
+  quizDifficulty.innerHTML = '';
+  quizQuestion.innerHTML = '再度チャレンジしたい場合は以下をクリック！';
+  title.innerText = `あなたの正解数は${score}です！！`;
 
   const returnBtn = document.createElement('button');
   returnBtn.innerText = 'ホームに戻る';
   quizOptionsContainer.appendChild(returnBtn);
   returnBtn.addEventListener('click', () => {
-
+    doReload();
   })
 }
 
